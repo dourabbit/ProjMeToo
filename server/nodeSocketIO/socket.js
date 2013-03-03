043dd0fbad9cc48748ef2c1bafc6a9459b6723f9
dourabbit <dourabbit>
2013-03-03 16:47:42 +0800

    Revert "failed in test"
    This reverts commit 2255ffc792bca97dcf097f87cb13e84393921a8d.

--- server/nodeSocketIO/socket.js
+++ server/nodeSocketIO/socket.js
@@ -1,36 +1,31 @@
-
-
-////Below codes failed in different computers
-//var net = require('net');
-//var tcp_server = net.createServer(function(socket){
 //
-//	socket.write('Server Connected\r\n');
-//	socket.pipe(socket);
+//var http = require('http'), io = require('socket.io');
+////创建server
+//var server = http.createServer(function(req, res){ 
+//  // Send HTML headers and message
+//  res.writeHead(200,{ 'Content-Type': 'text/html' }); 
+//  res.end('<h1>Hello Socket Lover!</h1>');
 //});
-//tcp_server.listen(1984,'127.0.0.1');
-
-
-//var io = require('socket.io').listen(1984);
-//
-//io.sockets.on('connection', function (socket) {
-//              socket.write('Server Connected\r\n');
-//              socket.pipe(socket);
-//              
-//              //连接成功则执行下面的监听
-//              socket.on('message',function(event){
-//                        console.log('Received message from client!',event);
-//              });
-//              //断开连接callback
-//              socket.on('disconnect',function(){
-//                        console.log('Server has disconnected');
-//              });
+////端口8000
+//server.listen(1985);
+////创建socket
+//var socket = io.listen(server);
+////添加连接监听
+//socket.on('connection', function(client){   
+//	//连接成功则执行下面的监听
+//	client.on('message',function(event){ 
+//		console.log('Received message from client!',event);
+//	});
+//	//断开连接callback
+//	client.on('disconnect',function(){
+//		console.log('Server has disconnected');
+//	});
 //});
 
-var io = require('socket.io').listen(3000);
+var net = require('net');
+var tcp_server = net.createServer(function(socket){
 
-io.sockets.on('connection', function (socket) {
-              socket.emit('news', { hello: 'world' });
-              socket.on('my other event', function (data) {
-                        console.log(data);
-                        });
-              });
+	socket.write('Echo hello from server \r\n');
+	socket.pipe(socket);
+});
+tcp_server.listen(1984,'127.0.0.1');
